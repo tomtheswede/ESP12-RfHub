7c5c6377e24f50fe28ece45995ab11ce861cdbf9
Thomas <tomtheswede@gmail.com>
2017-04-17 18:10:53 +0800

    Changed to suit new server. Working with AtTiny85 code

--- ESP12-RfHub.ino
+++ ESP12-RfHub.ino
@@ -1,21 +1,21 @@
+//Updated 16/4/17
+
 // Import ESP8266 libraries
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-const char* sensorID1 = "BUT008"; //Name of sensor
-const char* sensorID2 = "BUT009"; //Name of sensor
-const char* sensorID3 = "BUT010"; //Name of sensor
-const char* deviceDescription1 = "RemoteButtonA";
-const char* deviceDescription2 = "RemoteButtonB";
-const char* deviceDescription3 = "DoorSensor";
-const int readPin = 16;
+const unsigned long devID[3] = {24249122,12312414,12313}; //Name of sensor
+const unsigned long devType[3] = {5,13,8}; //Type of sensor is temperature
+
+const int readPin = 16; //16 for v0.1 of PWM board print
 
 //1 = 690 up then 310 down.  990 up, 370 down
 //0== 245 up then 775 down.   310 up, 1050 down
 const long t1=200;
 const long t2=600;
 const long t3=601;
-const long t4=1700;
+const long t4=1200;
+const byte msgLengths[4]={9,6,7,9};
 
 
 long lastRead=millis();
@@ -26,6 +26,7 @@ long fallTime=lastRead;
 long upTime=10;
 long downTime=10;
 bool fallFlag=false;
+bool riseFlag=false;
 bool newByte=false;
 byte bitCount=0;
 byte byteCount=0;
@@ -34,6 +35,8 @@ bool record=false;
 unsigned int msg=0;
 long lastTriggered=0;
 bool msgFlag=false;
+byte msgType=0;
+String outputString="";
 
 
 
@@ -58,13 +61,13 @@ void setup() {
   
   // put your setup code here, to run once:
   Serial.begin(115200);
-  ConnectWifi();
+  //ConnectWifi();  //Only include connection if in produduction mode
 
   delay(2000); //Time clearance to ensure registration
   //pinMode(indicatorPin,OUTPUT);
-  SendUdpValue("REG",sensorID1,String(deviceDescription1)); //Register LED on server
-  SendUdpValue("REG",sensorID2,String(deviceDescription2)); //Register LED on server
-  SendUdpValue("REG",sensorID3,String(deviceDescription3)); //Register LED on server
+  SendUdpValue(0,devID[0],devID[0]); //Register LED on server
+  SendUdpValue(0,devID[1],devID[1]); //Register LED on server
+  SendUdpValue(0,devID[2],devID[2]); //Register LED on server
 
 
 }
@@ -89,12 +92,13 @@ void ConnectWifi() {
 
 void loop() {
   //For detecting rising and falling edges of a waveform
-  if (micros()-lastRead>20) { //Takes 160us to fill buffer
+  if (micros()-lastRead>10) { //Takes 80us to fill buffer
     rfBuffer=rfBuffer<<1;
     rfBuffer=rfBuffer+digitalRead(readPin);
     if (rfBuffer==7) { //Indicates a rise 00001111
       riseTime=micros();
       downTime=riseTime-fallTime;
+      riseFlag=true;
     }
     else if (rfBuffer==240) { //Indicates a fall 11110000
       fallTime=micros();
@@ -103,18 +107,24 @@ void loop() {
     }
   }
   //For catching bit durations and adding to the buffer, then saying when a new byte is ready
-  if (fallFlag) {
-    fallFlag=false;
+  if (riseFlag) {
+    riseFlag=false;
     if (downTime>t4+t1) { //Message must have ended since no new bits detected
       bitBuffer=0;
       bitCount=0;
       byteCount=0;
+      if (record) { //stop recording if bits are wrong
+        record=false;
+      }
     }
     else if (upTime>t1 && upTime<t2) { //Indicates a 0
       bitBuffer=bitBuffer<<1;
       bitCount=bitCount+1;
-      if (bitCount>=8) {
-        bitCount=0;
+      //Serial.print("upTime,");     //Uncomment to get a read-out of rf pulse lengths
+      //Serial.println(upTime);        //Uncomment to get a read-out of rf pulse lengths
+      //Serial.print("downTime,");   //Uncomment to get a read-out of rf pulse lengths
+      //Serial.println(downTime);      //Uncomment to get a read-out of rf pulse lengths
+      if (bitCount>=8 && record) {
         newByte=true;
       }
     }
@@ -122,11 +132,23 @@ void loop() {
       bitBuffer=bitBuffer<<1;
       bitBuffer=bitBuffer+1;
       bitCount=bitCount+1;
-      if (bitCount>=8) {
-        bitCount=0;
+      //Serial.print("upTime,");     //Uncomment to get a read-out of rf pulse lengths
+      //Serial.println(upTime);        //Uncomment to get a read-out of rf pulse lengths
+      //Serial.print("downTime,");   //Uncomment to get a read-out of rf pulse lengths
+      //Serial.println(downTime);      //Uncomment to get a read-out of rf pulse lengths
+      if (bitCount>=8 && record) {
         newByte=true;
       }
     }
+    if ((bitBuffer==120 || bitBuffer==121 || bitBuffer==122 || bitBuffer==123) && (!record)) {
+      //Serial.println("Preamble met");
+      newByte=true;
+      record=true;
+      msgType=bitBuffer-120;
+    }
+    //else if (bitCount>127) {
+    //  bitCount=0;
+    //}
   }
   //For recognising if a preable criteria is met and starting record
   if (newByte) {
@@ -134,17 +156,16 @@ void loop() {
     //Serial.println(bitBuffer); //Uncomment to get a stream of read bytes----------
     if (record) {
       byteStore[byteCount]=bitBuffer;
+      bitBuffer=0;
+      bitCount=0;
       byteCount=byteCount+1;
-      if (byteCount>=2) {
-        msg=byteStore[0]*256+byteStore[1];
+      //Serial.println(byteCount);
+      if (byteCount>=msgLengths[msgType]) {
+        //Serial.println("-END-");
         msgFlag=true;
-        //Serial.println(msg);  //Uncomment to see each integer message------
         record=false;
+        byteCount=0;
       }
-    }
-    else if (bitBuffer==217 || bitBuffer==225) { //Preamble condition met. 225 is preamble for Door sensor
-      record=true;
-      //Serial.println("Preamble met");
     }
   }
   //Process messages
@@ -153,35 +174,47 @@ void loop() {
     if (millis()<lastTriggered) {
       lastTriggered=millis();
     }
-    if ((msg==22882) && millis()-lastTriggered>300) {
-      lastTriggered=millis();
-      //Serial.println("Button A triggered");
-      SendUdpValue("LOG",sensorID1,"press");
+    outputString="";
+    for (int i=0; i<msgLengths[msgType]; i++) {
+      outputString=outputString+byteStore[i]+',';
     }
-    if (msg==22884 && millis()-lastTriggered>300) {
-      lastTriggered=millis();
-      //Serial.println("Button B triggered");
-      SendUdpValue("LOG",sensorID2,"press");
-    }
-    if (msg==38726 && millis()-lastTriggered>10000) {
-      lastTriggered=millis();
-      //Serial.println("Door triggered");
-      SendUdpValue("LOG",sensorID3,"press");
-    }
+    Serial.println(outputString);
+    
+//    if ((msg==22882) && millis()-lastTriggered>300) {
+//      lastTriggered=millis();
+//      Serial.println("Button A triggered");
+//      SendUdpValue(1,devID[0],0);
+//    }
+//    if (msg==22884 && millis()-lastTriggered>300) {
+//      lastTriggered=millis();
+//      Serial.println("Button B triggered");
+//      SendUdpValue(1,devID[1],0);
+//    }
+//    if (msg==43904 && millis()-lastTriggered>300) {
+//      lastTriggered=millis();
+//      Serial.println("Remote 2 triggered");
+//      SendUdpValue(1,devID[1],0);
+//    }
+//    if (msg==38726 && millis()-lastTriggered>10000) {
+//      lastTriggered=millis();
+//      Serial.println("Door triggered");
+//      SendUdpValue(1,devID[2],0);
+//    }
   }
 }
 
-void SendUdpValue(String type, String sensorID, String value) {
-  //Print GPIO state in serial
+
+void SendUdpValue(byte type, unsigned long devID, unsigned long value) {
+  //Print GPIO state in //Serial
   Serial.print("-Value sent via UDP: ");
-  Serial.println(type + "," + sensorID + "," + value);
+  Serial.println(String(type) + "," + String(devID) + "," + String(value));
 
   // send a message, to the IP address and port
   Udp.beginPacket(ipAdd,localPort);
-  Udp.print(type);
+  Udp.print(String(type));
   Udp.write(",");
-  Udp.print(sensorID);
+  Udp.print(String(devID));
   Udp.write(",");
-  Udp.print(value); //This is the value to be sent
+  Udp.print(String(value)); //This is the value to be sent
   Udp.endPacket();
 }
