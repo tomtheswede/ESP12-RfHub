3ca5be9e40025717ad484dae384e35aa97221ac5
Thomas Friberg <tomtheswede@gmail.com>
2017-05-13 22:55:02 +0800

    Addition of button and other goodies

--- ESP12-RfHub.ino
+++ ESP12-RfHub.ino
@@ -1,20 +1,30 @@
-//Updated 17/4/17
+//Updated 18/4/17
 
 // Import ESP8266 libraries
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-const unsigned long devID[2] = {24249122,1231}; //Name of sensor
-const unsigned long devType[2] = {1,13}; //Type of sensor is button and RF server
+// WiFi parameters
+const char* ssid = "ThomasWifi"; //Enter your WiFi network name here in the quotation marks
+const char* password = "vanillamoon576"; //Enter your WiFi pasword here in the quotation marks
 
-const int readPin = 16; //16 for v0.1 of PWM board print
+//Server details
+unsigned int localPort = 5007;  //UDP send port
+const char* ipAdd = "192.168.0.100"; //Server address
+byte packetBuffer[512]; //buffer for incoming packets
+
+const int devices=2; //Number of LED devices
+const unsigned long devID[devices] = {31249122,23323231}; //Name of sensor
+const unsigned long devType[devices] = {31,35}; //Type of sensor is button and RF server
+
+const int devPin[devices] = {16,14}; //LED pin number 12 for LED2, 13 for LED1. 14 for RF and 16 for button.
 
 //1 = 690 up then 310 down.  990 up, 370 down
 //0== 245 up then 775 down.   310 up, 1050 down
-const long t1=200;
-const long t2=600;
-const long t3=601;
-const long t4=1200;
+const long t1=30;
+const long t2=300;
+const long t3=t2+1;
+const long t4=700;
 const byte msgLengths[4]={9,6,7,9};
 
 
@@ -44,14 +54,12 @@ boolean transmitFlag=false;
 long lastTransmit=0;
 
 
-// WiFi parameters
-const char* ssid = "ThomasWifi"; //Enter your WiFi network name here in the quotation marks
-const char* password = "vanillamoon576"; //Enter your WiFi pasword here in the quotation marks
-
-//Server details
-unsigned int localPort = 5007;  //UDP send port
-const char* ipAdd = "192.168.1.100"; //Server address
-byte packetBuffer[512]; //buffer for incoming packets
+//Button related
+bool lastButtonState=0;
+bool buttonState=0;
+long buttonTriggerTime=millis();
+long currentTime=millis();
+bool primer[4]={0,0,0,0};
 
 WiFiUDP Udp; //Instance to send packets
 
@@ -62,6 +70,9 @@ void setup() {
   digitalWrite(12, 0); //Turn off LED while connecting
   pinMode(13, OUTPUT); //Set as output
   digitalWrite(13, 0); //Turn off LED while connecting
+
+  //button setup
+  pinMode(devPin[devices],INPUT_PULLUP);
   
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -69,11 +80,10 @@ void setup() {
 
   delay(2000); //Time clearance to ensure registration
   //pinMode(indicatorPin,OUTPUT);
-  SendUdpValue(0,devID[0],devID[0]); //Register LED on server
-  SendUdpValue(0,devID[1],devID[1]); //Register LED on server
+  for (int i=0; i<devices;i++){
+    SendUdpValue(0,devID[i],devType[i]); //Register LED on server
+  }
   Serial.println("Ready");
-
-
 }
 
 void ConnectWifi() {
@@ -99,9 +109,9 @@ void loop() {
     ConnectWifi();
   }
   //For detecting rising and falling edges of a waveform
-  if (micros()-lastRead>10) { //Takes 80us to fill buffer
+  if (micros()-lastRead>10) { //Takes 56us to fill buffer
     rfBuffer=rfBuffer<<1;
-    rfBuffer=rfBuffer+digitalRead(readPin);
+    rfBuffer=rfBuffer+digitalRead(devPin[0]);
     if (rfBuffer==7) { //Indicates a rise 00001111
       riseTime=micros();
       downTime=riseTime-fallTime;
@@ -127,10 +137,6 @@ void loop() {
     else if (upTime>t1 && upTime<t2) { //Indicates a 0
       bitBuffer=bitBuffer<<1;
       bitCount=bitCount+1;
-      //Serial.print("upTime,");     //Uncomment to get a read-out of rf pulse lengths
-      //Serial.println(upTime);        //Uncomment to get a read-out of rf pulse lengths
-      //Serial.print("downTime,");   //Uncomment to get a read-out of rf pulse lengths
-      //Serial.println(downTime);      //Uncomment to get a read-out of rf pulse lengths
       if (bitCount>=8 && record) {
         newByte=true;
       }
@@ -139,10 +145,6 @@ void loop() {
       bitBuffer=bitBuffer<<1;
       bitBuffer=bitBuffer+1;
       bitCount=bitCount+1;
-      //Serial.print("upTime,");     //Uncomment to get a read-out of rf pulse lengths
-      //Serial.println(upTime);        //Uncomment to get a read-out of rf pulse lengths
-      //Serial.print("downTime,");   //Uncomment to get a read-out of rf pulse lengths
-      //Serial.println(downTime);      //Uncomment to get a read-out of rf pulse lengths
       if (bitCount>=8 && record) {
         newByte=true;
       }
@@ -153,9 +155,6 @@ void loop() {
       record=true;
       msgType=bitBuffer-120;
     }
-    //else if (bitCount>127) {
-    //  bitCount=0;
-    //}
   }
   //For recognising if a preable criteria is met and starting record
   if (newByte) {
@@ -182,11 +181,6 @@ void loop() {
     if (millis()<lastTriggered) {
       lastTriggered=millis();
     }
-    //Shows all bytes, comma seperated
-//    outputString="";
-//    for (int i=0; i<msgLengths[msgType]; i++) {
-//      outputString=outputString+byteStore[i]+',';
-//    }
     
     //In transmission form
     unsigned long dID = 0;
@@ -214,6 +208,44 @@ void loop() {
     }
     lastString2=lastString1;
     lastString1=outputString;
+  }
+}
+
+void CheckButton() {
+  buttonState=(!digitalRead(devPin[devices]));
+  currentTime=millis();
+  if (buttonState!=lastButtonState) {
+    if (buttonState && currentTime-buttonTriggerTime>300) {
+      SendUdpValue(1,devID[devices],1);
+      buttonTriggerTime=currentTime;
+      primer[0]=1;
+      primer[1]=1;
+      primer[2]=1;
+      primer[3]=1;
+    }
+    else if (!buttonState) {
+      primer[0]=0;
+      primer[1]=0;
+      primer[2]=0;
+      primer[3]=0;
+    }
+  }
+  lastButtonState=buttonState;
+  if (primer[0] && (currentTime-buttonTriggerTime>600)) {
+    SendUdpValue(1,devID[devices],2);
+    primer[0]=0;
+  }
+  else if (primer[1] && (currentTime-buttonTriggerTime>1500)) {
+    SendUdpValue(1,devID[devices],3);
+    primer[1]=0;
+  }
+  else if (primer[2] && (currentTime-buttonTriggerTime>4000)) {
+    SendUdpValue(1,devID[devices],4);
+    primer[2]=0;
+  }
+  else if (primer[3] && (currentTime-buttonTriggerTime>8000)) {
+    SendUdpValue(0,devID[devices],devType[devices]);  //Register
+    primer[3]=0;
   }
 }
 
